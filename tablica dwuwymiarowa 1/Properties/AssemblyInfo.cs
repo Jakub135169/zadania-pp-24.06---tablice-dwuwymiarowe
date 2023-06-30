b31a706972a1cd8cf549b11c6d32c90854b938ec
Jakub135169 <128227085+Jakub135169@users.noreply.github.com>
2023-06-30 20:29:18 +0200

    Initial commit

---
+++ tablica dwuwymiarowa 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("tablica dwuwymiarowa 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tablica dwuwymiarowa 1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("17a79d98-6d8c-4ce8-b95b-a41e9047d66d")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tablica dwuwymiarowa 3/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablica_dwuwymiarowa_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadanie 1:");
+            int[,] array1 = FillArrayAscendingDescending(10);
+            PrintArray(array1);
+
+            Console.WriteLine("\nZadanie 2:");
+            int[,] array2 = FillArraySpiral(10);
+            PrintArray(array2);
+
+            Console.WriteLine("\nZadanie 3:");
+            int size;
+            do
+            {
+                Console.Write("Podaj rozmiar tablicy: ");
+            } while (!int.TryParse(Console.ReadLine(), out size) || size <= 0);
+
+            int[,] array3 = FillArrayAscendingDescending(size);
+            PrintArray(array3);
+        }
+
+        static int[,] FillArrayAscendingDescending(int size)
+        {
+            int[,] array = new int[size, size];
+
+            int num = 1;
+            for (int i = 0; i < size; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        array[i, j] = num;
+                        num++;
+                    }
+                }
+                else
+                {
+                    for (int j = size - 1; j >= 0; j--)
+                    {
+                        array[i, j] = num;
+                        num++;
+                    }
+                }
+            }
+
+            return array;
+        }
+
+        static int[,] FillArraySpiral(int size)
+        {
+            int[,] array = new int[size, size];
+
+            int num = 1;
+            int rowStart = 0;
+            int rowEnd = size - 1;
+            int colStart = 0;
+            int colEnd = size - 1;
+
+            while (num <= size * size)
+            {
+                for (int i = colStart; i <= colEnd; i++)
+                {
+                    array[rowStart, i] = num;
+                    num++;
+                }
+
+                for (int i = rowStart + 1; i <= rowEnd; i++)
+                {
+                    array[i, colEnd] = num;
+                    num++;
+                }
+
+                for (int i = colEnd - 1; i >= colStart; i--)
+                {
+                    array[rowEnd, i] = num;
+                    num++;
+                }
+
+                for (int i = rowEnd - 1; i > rowStart; i--)
+                {
+                    array[i, colStart] = num;
+                    num++;
+                }
+
+                rowStart++;
+                rowEnd--;
+                colStart++;
+                colEnd--;
+            }
+
+            return array;
+        }
+
+        static void PrintArray(int[,] array)
+        {
+            int size = array.GetLength(0);
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
